ccc3438db09bb4ac6130532b2049aa7775f030e2
Vinay288 <vinayhiremath288@gmail.com>
2021-09-30 22:41:57 +0530

    ADDED: method to read and write from csv file

--- src/main/java/AddressBookIOService.java
+++ src/main/java/AddressBookIOService.java
@@ -1,3 +1,8 @@
+import com.opencsv.CSVReader;
+import com.opencsv.CSVWriter;
+
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -29,10 +34,53 @@ public class AddressBookIOService {
             throw new FileIOException(e.getMessage());
         }
     }
-    public void readFromFile(String fileName){
+
+    public void readFromFile(String fileName) {
         try {
             Files.lines(Paths.get(fileName.concat(".txt"))).forEach(System.out::println);
-        } catch (IOException e) {
+        } catch (Exception e) {
+            throw new FileIOException(e.getMessage());
+        }
+    }
+    public String[] getStringArray(Contact contact) {
+        String[] stringArray = new String[8];
+        stringArray[0] = contact.getFirstName();
+        stringArray[1] = contact.getLastName();
+        stringArray[2] = contact.getPlace().getCity();
+        stringArray[3] = contact.getAddress();
+        stringArray[4] = contact.getPlace().getState();
+        stringArray[5] = contact.getEmail();
+        stringArray[6] = String.valueOf(contact.getPlace().getZip());
+        stringArray[7] =Long.toString(contact.getPhoneNumber());
+        return stringArray;
+    }
+
+    public void writeToCSVFile(AddressBook addressBook, String fileName) {
+        try {
+            CSVWriter writer = new CSVWriter(new FileWriter(fileName.concat(".csv")));
+            List<String[]> contactsArrayList = new ArrayList();
+            for (Contact contact : addressBook.getContactsList()) {
+                contactsArrayList.add(getStringArray(contact));
+            }
+            writer.writeAll(contactsArrayList);
+            writer.close();
+        }  catch (Exception e) {
+            throw new FileIOException(e.getMessage());
+        }
+
+    }
+    public void readFromCSVFile(String fileName){
+        try {
+            FileReader filereader = new FileReader(fileName + ".csv");
+            CSVReader csvReader = new CSVReader(filereader);
+            String[] nextRecord;
+            while ((nextRecord = csvReader.readNext()) != null) {
+                for (String cell : nextRecord) {
+                    System.out.print(cell + "\t");
+                }
+                System.out.println();
+            }
+        } catch (Exception e) {
             throw new FileIOException(e.getMessage());
         }
     }
