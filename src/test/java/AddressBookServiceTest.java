6b56325b3655477995b055c30336e9f0032eca71
Vinay288 <vinayhiremath288@gmail.com>
2021-09-30 15:08:14 +0530

    ADDED: test to check if contact details are updated

--- src/test/java/AddressBookServiceTest.java
+++ src/test/java/AddressBookServiceTest.java
@@ -19,4 +19,15 @@ public class AddressBookServiceTest {
         addressBookService.addContactToAddressBook(addressBook,contact);
         Assertions.assertTrue(addressBook.getContactsList().contains(contact));
     }
+
+    @Test
+    public void givenAddressBookAndColumnToBeEdited_EditTheContact(){
+        AddressBookService addressBookService=new AddressBookService();
+        Contact contact=new Contact((int)(Math.random() * 100), "vinay", "hiremath", "street 2", 9874512420L, "vvj@gmail.com", "badami", "karnataka", 587201);
+        AddressBook addressBook=new AddressBook((int) (Math.random()*100),"AddressBook1");
+        addressBookService.addContactToAddressBook(addressBook,contact);
+        String updateValue="hiree";
+        addressBookService.editAddressBook(addressBook,"vinay",2,updateValue);
+        Assertions.assertEquals(updateValue,addressBookService.getContactfromAddressBook(addressBook,"vinay").getLastName());
+    }
 }
