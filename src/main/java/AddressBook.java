4a9b8fa1434acfcfdc4c3424955bdd7729867850
Vinay288 <vinayhiremath288@gmail.com>
2021-09-30 10:05:25 +0530

    Added: Addressbook class with attributes and constructor

---
+++ src/main/java/AddressBook.java
@@ -0,0 +1,47 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class AddressBook {
+    private final Integer id;
+    private String name;
+    private List<Contact> contactsList;
+    private List<AddressBookType> addressBookTypeList;
+
+    public List<AddressBookType> getAddressBookTypeList() {
+        return addressBookTypeList;
+    }
+
+    public void setAddressBookTypeList(List<AddressBookType> addressBookTypeList) {
+        this.addressBookTypeList = addressBookTypeList;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Contact> getContactsList() {
+        return contactsList;
+    }
+
+    public void setContactsList(List<Contact> contactsList) {
+        this.contactsList = contactsList;
+    }
+
+    AddressBook(Integer id, String name){
+        this.id=id;
+        this.name=name;
+        contactsList=new ArrayList<>();
+        addressBookTypeList=new ArrayList<>();
+    }
+    public void addContact(Contact contact){
+        contactsList.add(contact);
+    }
+}
