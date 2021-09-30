39b79210473be1def06deaed01827f891c01d017
Vinay288 <vinayhiremath288@gmail.com>
2021-09-30 22:48:21 +0530

    ADDED: test to write to json file

--- src/test/java/AddressBookFileIOTest.java
+++ src/test/java/AddressBookFileIOTest.java
@@ -39,4 +39,16 @@ public class AddressBookFileIOTest {
         boolean result = addressBookService.readService("vinn", IOService.CSV_IO);
         Assertions.assertTrue(result);
     }
+    @Test
+    public void whenGivenFileName_WhenCorrect_WriteToJSON() {
+        AddressBookService addressBookService = new AddressBookService();
+        Contact contact1 = new Contact((int) (Math.random() * 100), "vinay", "hiremath", "street 2", 9874512420L, "vvj@gmail.com", "badami", "karnataka", 587201);
+        Contact contact2 = new Contact((int) (Math.random() * 100), "vishal", "hiremath", "street 2", 9874512420L, "vvj@gmail.com", "bagalkot", "karnataka", 587201);
+        AddressBook addressBook = new AddressBook((int) (Math.random() * 100), "AddressBook1");
+        addressBookService.addAdderessBook(addressBook);
+        addressBookService.addContactToAddressBook(addressBook, contact1);
+        addressBookService.addContactToAddressBook(addressBook, contact2);
+        boolean result = addressBookService.writeService(addressBook.getName(), IOService.JSON_IO, "vinn");
+        Assertions.assertTrue(result);
+    }
 }
