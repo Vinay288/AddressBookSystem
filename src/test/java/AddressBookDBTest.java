6dc2c6f8e050479310d5ec301329804d7a10ab88
Vinay288 <vinayhiremath288@gmail.com>
2021-10-01 00:57:41 +0530

    ADDED: test to update contact

--- src/test/java/AddressBookDBTest.java
+++ src/test/java/AddressBookDBTest.java
@@ -1,4 +1,5 @@
 import org.junit.Assert;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
@@ -9,5 +10,16 @@ public class AddressBookDBTest {
         AddressBookService addressBookService = new AddressBookService();
         boolean result = addressBookService.readService("address_book1", IOService.DB_IO);
         Assert.assertTrue(result);
+    }
+    @Test
+    public void insertContactToGivenAddressBook_checkIfInserted() {
+        AddressBookService addressBookService = new AddressBookService();
+        Contact contact = new Contact(1897, "qwerty", "hii", "mangalore", 99787L, "karnataks@hhj", "mangalore", "kar",
+                87845);
+        addressBookService.createContact(contact);
+        addressBookService.writeService("address_book1",IOService.DB_IO,"");
+        addressBookService.readService("address_book1",IOService.DB_IO);
+        boolean result = addressBookService.compareContactSync("address_book1");
+        Assertions.assertTrue(result);
     }
 }
