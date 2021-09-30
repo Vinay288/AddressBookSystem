c414ffc12d7fa65feb4279d0611ea2ad49f99a8e
Vinay288 <vinayhiremath288@gmail.com>
2021-09-30 14:14:14 +0530

    ADDED: test to check if conatct if added to given addressbook

--- src/test/java/AddressBookServiceTest.java
+++ src/test/java/AddressBookServiceTest.java
@@ -6,7 +6,17 @@ public class AddressBookServiceTest {
     @Test
     public void givenContactDetails_whenCorrect_CreateContact() {
         AddressBookService addressBookService=new AddressBookService();
-        Contact createdContact=addressBookService.createContact((int)(Math.random() * 100), "vinay", "hiremath", "street 2", 9874512420L, "vvj@gmail.com", "badami", "karnataka", 587201);
-        Assertions.assertSame(createdContact, addressBookService.getContact());
+        Contact contact=new Contact((int)(Math.random() * 100), "vinay", "hiremath", "street 2", 9874512420L, "vvj@gmail.com", "badami", "karnataka", 587201);
+        addressBookService.createContact(contact);
+        Assertions.assertSame(contact, addressBookService.getContact());
+    }
+
+    @Test
+    public void givenAddresBookAndContact_whenCorrect_AddContactToAddressbook(){
+        AddressBookService addressBookService=new AddressBookService();
+        Contact contact=new Contact((int)(Math.random() * 100), "vinay", "hiremath", "street 2", 9874512420L, "vvj@gmail.com", "badami", "karnataka", 587201);
+        AddressBook addressBook=new AddressBook((int) (Math.random()*100),"AddressBook1");
+        addressBookService.addContactToAddressBook(addressBook,contact);
+        Assertions.assertTrue(addressBook.getContactsList().contains(contact));
     }
 }
