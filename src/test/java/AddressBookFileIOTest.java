e3b5929be61ac1f3a0eb3a640b0f3ce549981494
Vinay288 <vinayhiremath288@gmail.com>
2021-09-30 19:30:24 +0530

    ADDED: test to read from file

--- src/test/java/AddressBookFileIOTest.java
+++ src/test/java/AddressBookFileIOTest.java
@@ -3,15 +3,22 @@ import org.junit.jupiter.api.Test;
 
 public class AddressBookFileIOTest {
     @Test
-    public void whenGivenFileName_WhenCorrect_WriteToFile(){
-        AddressBookService addressBookService=new AddressBookService();
+    public void whenGivenFileName_WhenCorrect_WriteToFile() {
+        AddressBookService addressBookService = new AddressBookService();
         Contact contact1 = new Contact((int) (Math.random() * 100), "vinay", "hiremath", "street 2", 9874512420L, "vvj@gmail.com", "badami", "karnataka", 587201);
         Contact contact2 = new Contact((int) (Math.random() * 100), "vishal", "hiremath", "street 2", 9874512420L, "vvj@gmail.com", "bagalkot", "karnataka", 587201);
         AddressBook addressBook = new AddressBook((int) (Math.random() * 100), "AddressBook1");
         addressBookService.addAdderessBook(addressBook);
-        addressBookService.addContactToAddressBook(addressBook,contact1);
-        addressBookService.addContactToAddressBook(addressBook,contact2);
-        boolean result=addressBookService.writeService(addressBook.getName(),IOService.FILE_IO,"vinn");
+        addressBookService.addContactToAddressBook(addressBook, contact1);
+        addressBookService.addContactToAddressBook(addressBook, contact2);
+        boolean result = addressBookService.writeService(addressBook.getName(), IOService.FILE_IO, "vinn");
+        Assertions.assertTrue(result);
+    }
+
+    @Test
+    public void whenGivenFileName_whenCorrect_ReadFile() {
+        AddressBookService addressBookService = new AddressBookService();
+        boolean result = addressBookService.readService("vinn", IOService.FILE_IO);
         Assertions.assertTrue(result);
     }
 }
